313bbbae058eed4b96b0b386cdd3ed6abc9cff9d
Indra Wibisono <indrowibi@gmail.com>
2022-12-31 23:17:27 +0700

    Top Card

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,49 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:healthapp/common/constants.dart';
 
 class Body extends StatelessWidget {
-  final List<String> countries = [
-    "Brazil",
-    "Nepal",
-    "India",
-    "China",
-    "USA",
-    "Canada"
-  ];
-
-  Body({Key? key}) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: countries.map((country) {
-        return Container(
-            height: size.height * 0.18,
-            width: size.width * 0.9,
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            color: Colors.orangeAccent,
-            alignment: Alignment.center,
-            child: Text(country));
-      }).toList(),
+    return Container(
+      child: SizedBox(
+        height: size.height * 0.18,
+        child: PageView(scrollDirection: Axis.horizontal, children: [
+          TopCard(width: size.width),
+          TopCard(width: size.width),
+          TopCard(width: size.width)
+        ]),
+      ),
     );
   }
 }
 
-class TopCard extends StatelessWidget {
-  final double height;
-  const TopCard({Key? key, required this.height}) : super(key: key);
+class TopCard extends StatefulWidget {
+  final double width;
+
+  const TopCard({
+    super.key,
+    this.color = const Color(0xFFEBF0FC),
+    required this.width,
+    this.child,
+  });
+
+  final Color color;
+  final Widget? child;
 
   @override
+  State<TopCard> createState() => _TopCardState();
+}
+
+class _TopCardState extends State<TopCard> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height * 0.18,
-      margin: const EdgeInsets.symmetric(horizontal: 30),
-      decoration: const BoxDecoration(
-        color: Color(0xFFEBF0FC),
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
+    return SizedBox(
+      width: widget.width * 0.9,
+      child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          decoration: const BoxDecoration(
+            color: Color(0xFFEBF0FC),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          alignment: Alignment.center,
+          child: const Text("Test")),
     );
   }
 }
